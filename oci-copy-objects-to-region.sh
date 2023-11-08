2e3d7d0b15e39811a989b669151aaff4207a772c
fharris <fharris.dev@gmail.com>
2023-11-08 17:18:30 +0100

    Update oci-copy-objects-to-region.sh

--- oci-copy-objects-to-region.sh
+++ oci-copy-objects-to-region.sh
@@ -1,15 +1,23 @@
 #Requirements: oci cli installed and available in PATH
 #Documentation: https://docs.oracle.com/en-us/iaas/Content/Object/Tasks/copyingobjects.htm 
 
-export SBUCKET=bucket-velero
-export DBUCKET=bucket-velero
-export NAMESPACE=frsxwtjslf35
-export D_REGION_ID=uk-london-1
+export SBUCKET=source-bucket
+export DBUCKET=destination-bucket
+export NAMESPACE=objectstorage-namespace
+export D_REGION_ID=destination-region-id
 
-# List all objects in source bucket and export list to text file
-oci os object list --bucket-name=$SBUCKET --all | grep name | awk '{ print $2 }' | sed 's/"//g' | sed 's/,//g' > objects.list;
+# save object store list in a file
+oci os object list \
+--bucket-name=$SBUCKET --all | \
+grep name | awk '{ print $2 }' | sed 's/"//g' | sed 's/,//g' > object.list;
 
-# Bulk copy of all objects in text file to destination bucket in destination region:
-for id in $(cat objects.list)
-do oci os object copy --bucket-name $SBUCKET --source-object-name $id --destination-bucket $DBUCKET --namespace-name=$NAMESPACE  --destination-namespace=$NAMESPACE  --destination-region=$D_REGION_ID
+# Bulk objects copy to Destination region:
+for id in $(cat object.list)
+do oci os object copy \
+--bucket-name $SBUCKET \
+--source-object-name $id \
+--destination-bucket $DBUCKET \
+--namespace-name=$NAMESPACE  \
+--destination-namespace=$NAMESPACE  \
+--destination-region=$D_REGION_ID
 done
